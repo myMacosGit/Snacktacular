7cc88f4d4c19cd8ebeec7e18bdc9367bd4cf4aa4
myMacosGit <reg.isaacs@gmail.com>
2022-11-07 08:31:14 +0100

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Richard Isaacs on 07.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
